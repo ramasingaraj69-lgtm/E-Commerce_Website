55713e08ec6d19c69ea2bee0989aa366eb78ce9b
ramasingaraj69@gmail.com <ramasingaraj69@gmail.com>
2026-06-29 15:32:33 +0530

    Added Procfile for deployment

--- frontend/src/service/api.js
+++ frontend/src/service/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "https://e-commerce-website-zqw8.onrender.com",
+    baseURL: "https://e-commerce-website-zqw8.onrender.com/api",
 
 });
 
